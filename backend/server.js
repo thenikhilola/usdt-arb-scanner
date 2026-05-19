dc7ae733a5ad3a7ad60100338008a637c9e10a62
thenikhilola <70806694+thenikhilola@users.noreply.github.com>
2026-05-19 16:36:23 +0530

    Change server port from 4000 to 3000

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const crypto   = require("crypto");
 require("dotenv").config();
 
 const app  = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors({ origin: "*" }));
 app.use(express.json());
